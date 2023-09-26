93f012a5598b9c1c2b7c39b3f15c82382f5eb845
Osama <osama.altaf385@gmail.com>
2023-09-26 23:53:02 +0500

    changes in logo file

--- src/components/Header/logo.js
+++ src/components/Header/logo.js
@@ -6,7 +6,7 @@ import profileImg from "@/public/profile-img.png";
 const Logo = () => {
   return (
     <Link href="/">
-      <div>
+      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark">
         <Image
           src={profileImg}
           alt="Codebucks"
