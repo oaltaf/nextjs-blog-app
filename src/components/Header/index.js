4d798dd6b0ec77a617f2cddeb307b5099a185fd0
Osama <osama.altaf385@gmail.com>
2023-10-04 23:57:57 +0500

    changes in logo and index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Logo from "./logo";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="w-full p-4 px-10 flex items-center justify-between">
       <Logo />
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
     </header>
   );
 };
--- src/components/Header/logo.js
+++ src/components/Header/logo.js
@@ -6,7 +6,7 @@ import profileImg from "@/public/profile-img.png";
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark">
-      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark">
+      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark mr-4">
         <Image
           src={profileImg}
           alt="Codebucks"
