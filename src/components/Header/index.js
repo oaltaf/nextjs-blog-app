f0d0675ca9e1d86a7e4268cdc9035754ba24f1a6
Osama <osama.altaf385@gmail.com>
2023-10-06 18:58:10 +0500

    Icon.js added in components folder

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,33 @@
 import React from "react";
 import Logo from "./logo";
 import Link from "next/link";
+import { LinkedinIcon } from "../Icons";
 
 const Header = () => {
   return (
     <header className="w-full p-4 px-10 flex items-center justify-between">
       <Logo />
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/menu">Menu</Link>
+      <nav
+        className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize 
+      flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm"
+      >
+        <Link href="/" className="mr-2">
+          Home
+        </Link>
+        <Link href="/about" className="mx-2">
+          About
+        </Link>
+        <Link href="/contact" className="mr-2">
+          Contact
+        </Link>
+        <button>T</button>
       </nav>
+      <div>
+        <a href="http://example.com"><LinkedinIcon/></a>
+        <a href="http://example.com">Twitter</a>
+        <a href="http://example.com">Github</a>
+        <a href="http://example.com">Dribble</a>
+      </div>
     </header>
   );
 };
